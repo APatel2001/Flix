1c751c57474a9861c6685090cb23c49ebe91a551
Avi Patel <avidpatel1@gmail.com>
2022-01-30 01:59:14 -0500

    commit entire project

---
+++ Fllix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Fllix
+//
+//  Created by Avi Patel on 1/29/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
